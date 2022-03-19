f1cacffbedb8239f0a75fe5e1a546222578dd21b
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-19 03:47:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def indexpage():  
     return render_template('index.html')
 
+@app.route('/home')  
+def indexpage():  
+    return render_template('home.html')
+ 
+@app.route('/<name>')  
+def indexpage():  
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run()
