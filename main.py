19587e6f6b039a81982bb2a2c1d9f1d0f650cbee
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-19 04:09:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ app = Flask(__name__)
 @app.route('/')  
 def indexpage():  
     return render_template('index.html')
-
+ 
 @app.route('/home')  
 def indexpage():  
     return render_template('home.html')
- 
-@app.route('/<name>')  
-def indexpage():  
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
