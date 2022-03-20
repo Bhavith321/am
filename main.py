eb6813b05c54f8fab713d3c971f3b7268a50c820
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-20 21:17:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/')  
 def indexpage():  
     return render_template('index.html')
-   
-@app.route('/<search>')  
-def userpage(search):  
-    return render_template('index.html')
+
+@app.route('/<search>')
+def index(search):
+    return redirect(url_for('/'))
 
 if __name__ == '__main__':
     app.run()
