afa84358c45c329bb32bd7253435cbf239754225
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-20 06:28:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from flask import *
 
 app = Flask(__name__)  
+
+@app.route('/')
+def index():
+    return redirect(url_for('index'))
  
-@app.route('/')  
+@app.route('/index')  
 def indexpage():  
     return render_template('index.html')
- 
-@app.route('/home')  
-def loginpage():  
-    return render_template('home.html')
 
+@app.route('/index')
+def daccess():
+    abort(401)
+    this_is_never_executed()
+   
 @app.route('/<search>')  
 def userpage(search):  
     return render_template('index.html')
