a5875ad57276321811eedfd2e6163baeed988cf9
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-20 21:56:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,8 @@ from flask import *
 app = Flask(__name__)  
 
 @app.route('/')  
-def indexpage():  
-    return redirect(url_for('index'))
-
-@app.route('/index')  
 def rootpage():  
     return render_template('index.html')
-
-@app.route('/<search>')
-def searchpages(search):
-    return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run()
