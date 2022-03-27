1c055b5177146f45967ff53ab949d8106975fda9
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-27 21:49:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,22 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 def logiage():
    return render_template('login.html')
 
+@app.route('/index')
+def homeage():
+   return render_template('index.html')
+
 @app.route('/')
 def index():
     if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
+        return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         session['username'] = request.form['username']
         return redirect(url_for('index'))
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
+     return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
