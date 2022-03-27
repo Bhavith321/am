2c12e6ef94bea41fbf0e845d04a7b9009aa9e77a
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-27 22:22:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,37 +2,16 @@ from flask import *
 
 app = Flask(__name__)
 
-app.secret_key = b'_5#y2L4Q8z\n\xec]/'
-
 @app.route('/')
 def index():
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
+    return redirect(url_for('home'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
-
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('index'))
-
-@app.route('/index')
-def indexingh():
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 @app.route('/login')
-def indexh():
+def login():
     return render_template('login.html')
 
 if __name__ = '__main__':
