221ba022615dcf1219f86c816b5408b1a885453b
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-27 20:41:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,16 @@ def homepe():
 def logiage():
    return render_template('login.html')
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST' and
+   request.form['username'] == 'admin' :
+   return redirect(url_for('dashboard'))
+   return redirect(url_for('index'))
+
+@app.route('/dashboard')
+def success():
+   return 'logged in successfully'
+
 if __name__ == '__main__':
    app.run()
