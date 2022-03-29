550c4243a3a7799f3b1e511c065e667c9c662f2a
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-29 09:40:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def indx():
     return render_template('index.html')
 
 @app.route('/home')
-def home():
+def ggome():
     return render_template('index.html')
 
 @app.route('/login')
-def login():
+def loginiiu():
     return render_template('login.html')
 
 if __name__ = '__main__':
