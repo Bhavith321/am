326dc41f77a97047a200e499cc06f61dd5665d84
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-29 12:58:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/dashboard')
+def login():
+   return render_template('dashboard.html')
+
 if __name__ == '__main__':
    app.run()
