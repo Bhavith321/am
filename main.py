b4159b0d88ea4b5f932f73a014f253bb6e405f8a
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-03-31 20:15:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.route('/home')
+def homep():
+   return render_template('index.html')
+
 @app.route('/login')
 def login():
    return render_template('login.html')
@@ -12,6 +16,10 @@ def login():
 @app.route('/dashboard')
 def dashboard():
    return render_template('dashboard.html')
+
+@app.route('/<search>')
+def show_search(search):
+    return redirect(url_for(('home'))
 
 if __name__ == '__main__':
    app.run()
