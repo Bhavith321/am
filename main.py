951eda26b97b8fe7678987507093f89fca2b4ab8
BHAVITH WEB DEVELOPEMNT <101874065+Bhavith321@users.noreply.github.com>
2022-04-02 00:06:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ app = Flask(__name__)
 def index():
    return redirect(url_for('home'))
 
-@app.route('/home')
+@app.route('/home/')
 def home():
    return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login/')
 def login():
    return render_template('login.html')
 
-@app.route('/dashboard')
+@app.route('/dashboard/')
 def dashboard():
    return render_template('dashboard.html')
 
+@app.route('/verification/')
+def verification():
+   return render_template('verification.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
